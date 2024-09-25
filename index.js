70e3c950770e73bd0ad9fda6ed22b9c336c3be7f
Mohamed <mosalman1098@gmail.com>
2024-09-25 17:31:00 +0400

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 // Express Setup
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Serve static files (for HLS streams)
 app.use(express.static(path.join(__dirname, "public")));
